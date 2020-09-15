2a822d8e8afc8d5be99d992bc11f3f704abbaef8
wt <wangtao9009>
2020-09-15 16:12:49 +0800

    `1

--- pages/mydata/pageInfo/getMainText.js
+++ pages/mydata/pageInfo/getMainText.js
@@ -50,4 +50,44 @@ export const getMainText = (url, code) => {
 	}).catch((e) => {})
 }
 
+// 安卓沉浸式 0关闭 1安卓状态栏导航栏都进入沉浸模式 2设置状态栏沉浸 3设置导航栏沉浸   4显示返回键
+export function windowFull(pr) {  
+    var main = plus.android.runtimeMainActivity();  
+    var windowMe = main.getWindow();  
+    plus.android.importClass(windowMe);  
+    var decorView = windowMe.getDecorView();  
+    plus.android.importClass(decorView);  
+    var View = plus.android.importClass("android.view.View");  
+    if (pr === 1) {  
+        decorView.setSystemUiVisibility(  
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE |  
+            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |  
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |  
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |  
+            View.SYSTEM_UI_FLAG_FULLSCREEN |  
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);  
+    } else if (pr === 2) {  
+        decorView.setSystemUiVisibility(  
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE |  
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |  
+            View.SYSTEM_UI_FLAG_FULLSCREEN |  
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);  
+    } else if (pr === 3) {  
+        decorView.setSystemUiVisibility(  
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE |  
+            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |  
+            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |  
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);  
+    } else if (pr === 4) {  //显示返回键
+        decorView.setSystemUiVisibility(  
+		View.SYSTEM_UI_FLAG_LAYOUT_STABLE | 
+		//View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | //返回后覆盖tab 表示会让应用的主体内容占用系统导航栏的空间
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN  
+			);  
+    } else if (pr === 0) {  
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);  
+    }  
+}
+
+
  
